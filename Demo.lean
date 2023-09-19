07c63242fc71c13de498496f2ba74a846d412c5b
Alex J. Best <alex.j.best@gmail.com>
2023-09-19 19:30:06 +0100

    wip: adding code interpolation support

--- Demo.lean
+++ Demo.lean
@@ -1,5 +1,14 @@
 import LeanSlides
+import Lean
 
+open Lean
+#eval 1 + 1
+
+
+def aaa := 2
+
+def typeOf {α : Sort _} (a : α) := α 
+-- set_option pp.rawOnError true
 #slides Introduction /-!
 % Lean Slides
 % https://github.com/0art0/lean-slides/
@@ -17,14 +26,30 @@ from Markdown comments in the Lean editor.
 tutorials or demonstrations
 to avoid switching between 
 the slides and the Lean editor.
+
 -/
 
 -- Some intervening Lean code
 example : 1 = 1 := by
   rfl
 
+def rainbow (s : String) : String := 
+Id.run
+  do
+    let mut out := ""
+    let mut i := 0
+    let mut len : Float := 0
+    for _ in s.data do
+      len := len + 1 -- ew
+    for char in s.data do
+      let r : Float := (256 : Float) * i / len
+      out := out.append s!"<text style='color: rgb({r},{r},{r})'>{char}</text>"
+      i := i + 1
+    return out
+
 #slides Tool /-!
 # Dependencies
+
 
 `Lean Slides` works by combining together several tools:
 
@@ -62,17 +87,24 @@ as a [`Widget`](https://github.com/EdAyers/ProofWidgets4).
 
 The tool also features a code action to 
 go in and out of draft mode.
+
+<text style='color: red'>color</text>
+
+We can also evaluate {" ".intercalate ["arbitrary", "Lean", "code"]}
+We can also evaluate {rainbow "and make cool effects using Lean itself"}
 -/
 
--- Some more intervening Lean code
-#check Nat.succ
 
-#slides +draft Math /-!
+-- Some more intervening Lean code
+
+namespace LeanSlides
+
+#slides Math /-!
 # Rendering math
 
 The generated `reveal.js` slides
 render mathematics by default
-using $\KaTeX$.
+using $KaTeX$.{1}
 
 -/
 
--- LeanSlides.lean
+++ LeanSlides.lean
@@ -1,6 +1,8 @@
 import ProofWidgets.Component.HtmlDisplay
 import Std.CodeAction.Misc
+import Std.Tactic.OpenPrivate
 import LeanSlides.Init
+import Lean
 
 open Lean ProofWidgets Elab Parser Command Server System
 
@@ -28,9 +30,159 @@ def System.FilePath.getRelativePath (filePath : FilePath) : String :=
   else 
     panic! s!"The file path {filePath} is not a relative path."
 
-def extractModuleDocContent : TSyntax ``moduleDoc → String
-  | ⟨.node _ _ #[_, .atom _ doc]⟩ => doc.dropRight 2
-  | _ => panic! "Ill-formed module docstring."
+
+namespace Lean.Elab.Term
+open Meta
+
+open Lean
+
+private def mkEvalInstCore (evalClassName : Name) (e : Expr) : MetaM Expr := do
+  let α    ← inferType e
+  let u    ← getDecLevel α
+  let inst := mkApp (Lean.mkConst evalClassName [u]) α
+  try
+    synthInstance inst
+  catch _ =>
+    -- Put `α` in WHNF and try again
+    try
+      let α ← whnf α
+      synthInstance (mkApp (Lean.mkConst evalClassName [u]) α)
+    catch _ =>
+      -- Fully reduce `α` and try again
+      try
+        let α ← reduce (skipTypes := false) α
+        synthInstance (mkApp (Lean.mkConst evalClassName [u]) α)
+      catch _ =>
+        throwError "expression{indentExpr e}\nhas type{indentExpr α}\nbut instance{indentExpr inst}\nfailed to be synthesized, this instance instructs Lean on how to display the resulting value, recall that any type implementing the `Repr` class also implements the `{evalClassName}` class"
+
+private def mkRunMetaEval (e : Expr) : MetaM Expr :=
+  withLocalDeclD `env (Lean.mkConst ``Lean.Environment) fun env =>
+  withLocalDeclD `opts (Lean.mkConst ``Lean.Options) fun opts => do
+    let α ← inferType e
+    let u ← getDecLevel α
+    let instVal ← mkEvalInstCore ``Lean.MetaEval e
+    let e := mkAppN (Lean.mkConst ``Lean.runMetaEval [u]) #[α, instVal, env, opts, e]
+    instantiateMVars (← mkLambdaFVars #[env, opts] e)
+
+private def mkRunEval (e : Expr) : MetaM Expr := do
+  let α ← inferType e
+  let u ← getDecLevel α
+  let instVal ← mkEvalInstCore ``Lean.Eval e
+  instantiateMVars (mkAppN (Lean.mkConst ``Lean.runEval [u]) #[α, instVal, mkSimpleThunk e])
+-- open private Lean.Elab.Command.mkRunMetaEval from Lean.Elab.Command
+open Expr in
+-- TODO an α version would be cool in presence of Quote α 
+unsafe def metaEvalTerm /-(type : Expr)-/ (term : Syntax) /-(safety := DefinitionSafety.safe)-/ : CommandElabM String := do
+  let declName := `_eval
+  let addAndCompile (value : Expr) : TermElabM Unit := do
+    let value ← Term.levelMVarToParam (← instantiateMVars value)
+    let type ← inferType value
+    let us := collectLevelParams {} value |>.params
+    let value ← instantiateMVars value
+    let decl := Declaration.defnDecl {
+      name        := declName
+      levelParams := us.toList
+      type        := type
+      value       := value
+      hints       := ReducibilityHints.opaque
+      safety      := DefinitionSafety.unsafe
+    }
+    Term.ensureNoUnassignedMVars decl
+    addAndCompile decl
+  -- Elaborate `term`
+  let elabEvalTerm : TermElabM Expr := do
+    let e ← Term.elabTerm term none
+    Term.synthesizeSyntheticMVarsNoPostponing
+    if (← Term.logUnassignedUsingErrorInfos (← getMVars e)) then throwAbortTerm
+    if (← isProp e) then
+      mkDecide e
+    else
+      return e
+  -- Evaluate using term using `MetaEval` class.
+  let elabMetaEval : CommandElabM String := do
+    -- act? is `some act` if elaborated `term` has type `CommandElabM α`
+    let act? ← runTermElabM fun _ => Term.withDeclName declName do
+      let e ← elabEvalTerm
+      let eType ← instantiateMVars (← inferType e)
+      if eType.isAppOfArity ``CommandElabM 1 then
+        let mut stx ← Term.exprToSyntax e
+        unless (← isDefEq eType.appArg! (Lean.mkConst ``Unit)) do
+          stx ← `($stx >>= fun v => IO.println (repr v))
+        let act ← Lean.Elab.Term.evalTerm (CommandElabM String) (mkApp (Lean.mkConst ``CommandElabM) (Lean.mkConst ``String)) stx
+        pure <| some act
+      else
+        let e ← mkRunMetaEval e
+        let env ← getEnv
+        let opts ← getOptions
+        let act ← try addAndCompile e; evalConst (Environment → Options → IO (String × Except IO.Error Environment)) declName finally setEnv env
+        let (out, res) ← act env opts -- we execute `act` using the environment
+        return out
+        -- match res with
+        -- | Except.error e => throwError e.toString
+        -- | Except.ok env  => do setEnv env; pure none
+    let some act := act? | throwError "couldn't interpret as meta string"
+    act
+  -- Evaluate using term using `Eval` class.
+  let elabEval : CommandElabM String := runTermElabM fun _ => Term.withDeclName declName do
+    -- fall back to non-meta eval if MetaEval hasn't been defined yet
+    -- modify e to `runEval e`
+    let e ← mkRunEval (← elabEvalTerm)
+    let env ← getEnv
+    let act ← try addAndCompile e; evalConst (IO (String × Except IO.Error Unit)) declName finally setEnv env
+    let (out, res) ← liftM (m := IO) act
+    return out
+    -- logInfoAt term out -- was tk
+    -- match res with
+    -- | Except.error e => throwError e.toString
+    -- | Except.ok _    => pure ()
+  if (← getEnv).contains ``Lean.MetaEval then do
+    elabMetaEval
+  else
+    elabEval
+  -- let v ← elabTermEnsuringType value type
+  -- synthesizeSyntheticMVarsNoPostponing
+  -- let v ← instantiateMVars v
+  -- if (← logUnassignedUsingErrorInfos (← getMVars v)) then throwAbortTerm
+  -- if eType.isAppOfArity ``CommandElabM 1 then
+  --   let mut stx ← Term.exprToSyntax e
+  --   unless (← isDefEq eType.appArg! (mkConst ``Unit)) do
+  --     stx ← `($stx >>= fun v => IO.println (repr v))
+  --   let act ← Lean.Elab.Term.evalTerm (CommandElabM Unit) (mkApp (mkConst ``CommandElabM) (mkConst ``Unit)) stx
+  --   pure <| some act
+  -- evalExpr α type v safety
+
+end Lean.Elab.Term
+open Lean.Elab.Term
+
+unsafe
+def extractModuleDocContent : TSyntax ``moduleDoc → CommandElabM String
+  | ⟨.node _ _ #[_, .atom _ doc]⟩ => do
+      let e ←getEnv 
+      logInfo doc
+      -- TODO need escaping here for "" but unclear how
+      -- let doc := doc.replace "\"" "\\\""
+      -- let categories := (parserExtension.getState e).categories.toList.map (·.fst)
+      -- logInfo (toString categories)
+      let a ← match Parser.runParserCategory e `term <| "s!\"" ++ doc.dropRight 2 ++ "\"" with
+      | Except.ok a => do pure a
+      | Except.error s => throwError s
+      
+      let a : TSyntax interpolatedStrKind := ⟨a[1]⟩
+      let b ← liftMacroM <| Lean.TSyntax.expandInterpolatedStr a (← `(String)) (← `(toString))
+      logInfo b
+      -- logInfo (← liftCoreM (Meta.liftMetaM (Meta.whnf (← liftTermElabM <| Term.elabTerm b none))))
+      logInfo (← liftTermElabM <| Term.evalTerm String (mkConst ``String) b)
+      -- liftTermElabM <| Term.evalTerm String (mkConst ``String) b
+      metaEvalTerm b
+      -- liftTermElabM
+      --   (Lean.Meta.evalExpr String (← liftTermElabM <| Term.elabTerm b (mkConst ``String)) (mkConst ``String))--.run' {env := e}
+      
+      -- match (← liftTermElabM <| Term.elabTerm b none) with
+      -- | .lit (.strVal b) => return
+      -- (Lean.Meta.reduceEval b)
+      -- | _ => throwError "boo"
+      -- return (toString  b)
+  | _ => panic!"Ill-formed module docstring."
 
 def createMarkdownFile (title text : String) : IO FilePath := do
   let mdFile := markdownDir / (title ++ ".md")
@@ -82,16 +234,28 @@ def getSlidesFor (title : String) (content : String) : IO FilePath := do
 
 end Caching
 
+/-!"
+
+  # ad a 
+"-/
+section Doc
+
+open Lean Parser
+def slideContent := leading_parser ppDedent <|
+  "/-!" >> commentBody >> ppLine
+
+end Doc
+
 section Widget
 
 syntax (name := slidesCmd) "#slides" ("+draft")? ident moduleDoc : command
 
-@[command_elab slidesCmd] def revealSlides : CommandElab
+@[command_elab slidesCmd] unsafe def revealSlides : CommandElab
   | stx@`(command| #slides $title $doc) => do
     let name := title.getId.toString
-    let content := extractModuleDocContent doc
+    let content ← extractModuleDocContent doc
     let slidesPath ← getSlidesFor name content
-    let slidesUrl := (← getServerUrl)  ++ slidesPath.getRelativePath
+    let slidesUrl := (← getServerUrl) ++ slidesPath.getRelativePath
     IO.println s!"Rendering results for {name} hosted at {slidesUrl} ..."
     -- TODO: Check whether the server is up programmatically
     IO.println "Ensure that the server is running ..."
